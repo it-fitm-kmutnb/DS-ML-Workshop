2df20bec97a4437fe7500bc43c27cb51de3136a4
it-fitm-kmutnb <it.kmutnb@itm.kmutnb.ac.th>
2026-04-24 23:22:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ st.write("##### Module 1: การจัดการข้อมูลพื้
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
+
+if st.button("🛑 ระบบจำลอง Early Stopping"):
+    st.switch_page("pages/auto_stop_training.py")
