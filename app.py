df644a66c5cc010a1d1e2e40780163f4a681e3da
it-fitm-kmutnb <it.kmutnb@itm.kmutnb.ac.th>
2026-04-24 11:57:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Multi-page App", layout="wide")
 st.title("🏠 หน้าหลัก")
 st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
-st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
+st.write("##### Module 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
